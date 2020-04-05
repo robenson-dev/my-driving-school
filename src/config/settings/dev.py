503b97cc6c131e541a9a7e444429712e3ec99584
Pierre Robenson <robenson1.pierre@epitech.eu>
2020-04-05 22:02:36 +0200

    dev mood

---
+++ src/config/settings/dev.py
@@ -0,0 +1,14 @@
+from .defaults import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'database/db.sqlite3'),
+    }
+}
+
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost']
+
+SECRET_KEY = 'yy1(ruls3vkcfz%)-rafx_rbgw%jm&@w+cdxrhi&hsek1^war4'
+
+DEBUG = True
