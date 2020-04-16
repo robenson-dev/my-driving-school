d2848af4cfe820e271bf49e39c4410e7e7c02492
Pierre Robenson <robenson1.pierre@epitech.eu>
2020-04-16 08:12:30 +0200

    add signal for create auto account
    - student
    - instructor

--- src/users/signals.py
+++ src/users/signals.py
@@ -1,15 +1,21 @@
 from django.db.models.signals import post_save
-from users.models import User, Instructor
+from users.models import User, Instructor, Student
 from django.dispatch import receiver
 
 
-# @receiver(post_save, sender=User)
-# def create_instructor(sender, instance, created, **kwargs):
-#     if created:
-#         if instance.is_instructor:
-#             Instructor.objects.create(user=instance)
-#
-#
+@receiver(post_save, sender=User)
+def create_instructor(sender, instance, created, **kwargs):
+    if created:
+        if instance.is_instructor == True:
+            Instructor.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def create_instructor(sender, instance, created, **kwargs):
+    if created:
+        if instance.is_student == True:
+            Student.objects.create(user=instance)
+
 # @receiver(post_save, sender=User)
 # def save_instructor(sender, instance, created, **kwargs):
 #     instance.instructor.save()
